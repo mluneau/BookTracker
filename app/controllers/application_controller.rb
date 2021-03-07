cf65b0e7f388652932c5083be37101e23e781149
Maxence Luneau <maxenceluneau@gmail.com>
2021-03-07 18:08:35 +0100

    Create basic index layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  BOOK_CATEGORIES = ['Travel', 'Fantastic', 'Historic', 'Thriller', 'Self-Development', 'Children']
+
 end
