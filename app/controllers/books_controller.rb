e153dce14448003bf34ce5d743ae21a346ad4123
Maxence Luneau <maxenceluneau@gmail.com>
2021-03-07 17:51:47 +0100

    Add private functions for Books and Readings controllers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,33 @@
 class BooksController < ApplicationController
+  before_action :find_book, only: [:show]
+
   def index
+    @books = Book.all
   end
 
-  def show
+  def show  
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit([:title, :author, :description, :category, :language])
   end
 end
--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,4 +1,7 @@
 class ReadingsController < ApplicationController
+  before_action :find_book, only: [:new, :create]
+  before_action :find_reading, only: [:index, :show]
+
   def index
   end
 
@@ -12,5 +15,19 @@ class ReadingsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def find_book
+    @book = Book.find(params[:book_id])
+  end
+
+  def find_reading
+    @reading = Reading.find(params[:id])
+  end
+
+  def reading_params
+    params.require(:reading).permit(:start_date, :end_date, :rating, :status)
   end
 end
