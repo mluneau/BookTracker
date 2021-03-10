308910c34ca1d2e73e35c0903aac17c1fad31896
Maxence Luneau <maxenceluneau@gmail.com>
2021-03-10 22:48:18 +0100

    Create books/new basic form input

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   BOOK_CATEGORIES = ['Travel', 'Fantastic', 'Historic', 'Thriller', 'Self-Development', 'Children']
+  LANGUAGES = ['English', 'French', 'German', 'Spanish', 'Chinese', 'Arabic']
 
 end
